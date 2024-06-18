04c426d2cd11898c7eec48ca4c3e11c264e7faaf
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-06-18 17:39:31 +0900

    240618 오후자바 수업

---
+++ src/afternoon/variable/Var2Number.java
@@ -0,0 +1,13 @@
+package afternoon.variable;
+
+public class Var2Number {
+    public static void main(String[] args) {
+
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 9223372036854775807L;
+        float f = 3.14159265358979f;
+        double d = 3.14159265358979d;
+    }
+}
---
+++ src/afternoon/variable/Var3.java
@@ -0,0 +1,8 @@
+package afternoon.variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        double a = 0.7;
+        int c = 1;
+    }
+}
